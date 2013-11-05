47eec767739ba0e4c5bbbda0d4c96936fd558010
unknown <iguest@is-m440m27-27.ischool.uw.edu>
2013-11-05 14:19:26 -0800

    first page changed

--- js/demo.js
+++ js/demo.js
@@ -1,22 +1,70 @@
 $(function(){
-    renderPizza(com.dawgpizza.menu.pizzas, $('.pizza-template'), $('.menu-data1'));
+    renderPizza1(com.dawgpizza.menu.pizzas, $('.pizza-template'), $('.menu-data1'));
+    renderPizza2(com.dawgpizza.menu.pizzas, $('.pizza2-template'), $('.menu-data4'));
     renderDrink(com.dawgpizza.menu.drinks, $('.drink-template'), $('.menu-data2'));
     renderDessert(com.dawgpizza.menu.desserts, $('.dessert-template'), $('.menu-data3'));
 })
 
-function renderPizza(pizzas, template, menuData) {
+function renderPizza1(pizzas, template, menuData) {
     var instance;
     var idx;
     var pizza;
 
-    for(idx = 0; idx < com.dawgpizza.menu.pizzas.length; ++idx) {
+    for(idx = 0; idx < com.dawgpizza.menu.pizzas.length/2; ++idx) {
         instance = template.clone();
         pizza = com.dawgpizza.menu.pizzas[idx];
         instance.find('.name').html(pizza.name);
         instance.find('.description').html(pizza.description);
-        instance.find('.price').html('$' + pizza.prices[0] + ' / ' + '$' + pizza.prices[1]
-            + ' / ' + '$' + pizza.prices[2]);
+        instance.find('.price').html('$' + pizza.prices[0] + '/' + '$' + pizza.prices[1]
+            + '/' + '$' + pizza.prices[2]);
         instance.removeClass('template');
         menuData.append(instance);
     }
 } 
+
+function renderPizza2(pizzas, template, menuData) {
+    for(idx = com.dawgpizza.menu.pizzas.length/2; idx < com.dawgpizza.menu.pizzas.length; ++idx) {
+        instance = template.clone();
+        pizza = com.dawgpizza.menu.pizzas[idx];
+        instance.find('.name').html(pizza.name);
+        instance.find('.description').html(pizza.description);
+        instance.find('.price').html('$' + pizza.prices[0] + '/' + '$' + pizza.prices[1]
+            + '/' + '$' + pizza.prices[2]);
+        instance.removeClass('template');
+        menuData.append(instance);
+    }
+}
+
+function renderDrink(drinks, template, menuData) {
+    var instance;
+    var idx;
+    var drink;
+
+    for(idx = 0; idx < com.dawgpizza.menu.drinks.length; ++idx) {
+        instance = template.clone();
+        drink = com.dawgpizza.menu.drinks[idx];
+
+        instance.find('.name').html(drink.name);
+
+        instance.find('.price').html('$' + drink.price);
+        instance.removeClass('template');
+        menuData.append(instance);
+    }
+}
+
+function renderDessert(desserts, template, menuData) {
+    var instance;
+    var idx;
+    var dessert;
+
+    for(idx = 0; idx < com.dawgpizza.menu.drinks.length; ++idx) {
+        instance = template.clone();
+        dessert = com.dawgpizza.menu.desserts[idx];
+
+        instance.find('.name').html(dessert.name);
+
+        instance.find('.price').html('$' + dessert.price);
+        instance.removeClass('template');
+        menuData.append(instance);
+    }
+}
